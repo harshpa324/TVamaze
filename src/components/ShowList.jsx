c3edda993e8a977903cab72e347f999b90fe9a8c
harshpa324 <harshpa324@gmail.com>
2024-02-01 16:36:08 +0530

    finished for now

--- src/components/ShowList.jsx
+++ src/components/ShowList.jsx
@@ -2,25 +2,25 @@ import Link from 'next/link';
 
 const ShowList = ({ shows }) => {
   return (
-    <div className="grid grid-cols-3 gap-4">
-      
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
       {shows.map((show) => (
         <div key={show.show.id} className="bg-white p-4 shadow-md rounded-md">
-          <Link href={`/show/${show.show.id}`} 
-             className="text-blue-500">
-          <div className=" grid grid-cols-2 gap-3 ">
-            <img
-            src={show.show.image?.medium || 'https://placehold.it/210x295'}
-            alt={show.show.name}
-            className="w-210 h-295 object-cover mb-4 rounded-md"
-            />
-            <div className="flex flex-col justify-center items-center">
-              <p className="text-white bg-blue-500 p-1 mb-2">Language: {show.show.language}</p>
-              <p className="text-gray-400 text-sm">Genre: {show.show.genres.join(', ')}</p>
+          <Link href={`/show/${show.show.id}`} className="text-blue-500">
+            <div className="flex flex-col items-center">
+              <img
+                src={show.show.image?.medium || 'https://placehold.it/210x295'}
+                alt={show.show.name}
+                className="w-210 h-295 object-cover mb-4 rounded-md"
+              />
+              <div className="flex flex-col items-center">
+                <p className="text-white bg-blue-500 p-1 mb-2">Language: {show.show.language}</p>
+                <p className="text-gray-400 text-sm">Genre: {show.show.genres.join(', ')}</p>
+              </div>
             </div>
-          </div>
+          </Link>
           <h2 className="text-lg font-semibold mb-2">{show.show.name}</h2>
-          View Details
+          <Link href={`/show/${show.show.id}`} className="text-blue-500">
+            View Details
           </Link>
         </div>
       ))}
